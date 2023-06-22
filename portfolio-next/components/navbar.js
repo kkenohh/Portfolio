89f2d2456bc2c87546b6228fbcac96354b22c96c
Ken <kenoh17@uw.edu>
2023-06-22 00:37:08 -0700

    styled hidden navbar

--- portfolio-next/components/navbar.js
+++ portfolio-next/components/navbar.js
@@ -2,19 +2,37 @@ import styles from '@/styles/navbar.module.css'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import clsx from 'clsx'
+import { useState } from 'react';
 
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className={styles.nav}>
-      <a href="">
-        <FontAwesomeIcon icon={faBars} size='2x' className={styles.icon}/>
-      </a>
-      <div>
-        <div className={styles.socials}>
-          <a href="https://www.linkedin.com/in/ken-oh-64bb66239/" className={styles.link} target="_blank"><FontAwesomeIcon icon={faLinkedin} size='2x' className={styles.icon} /></a>
-          <a href="https://www.linkedin.com/in/ken-oh-64bb66239/" className={styles.link} target="_blank"><FontAwesomeIcon icon={faGithub} size='2x' className={styles.icon} /></a>
+      <div className={styles.header}>
+        <div className={styles.bars + ' ' + clsx({
+          [styles.darker]: menuOpen === true,
+          [styles.normal]: menuOpen === false})} onClick={() => setMenuOpen(!menuOpen)}>
+          <FontAwesomeIcon icon={faBars} size='2x' className={styles.icon} />
         </div>
+        <div>
+          <div className={styles.socials}>
+            <a href="https://www.linkedin.com/in/ken-oh-64bb66239/" className={styles.link} target="_blank"><FontAwesomeIcon icon={faLinkedin} size='2x' className={styles.icon} /></a>
+            <a href="https://github.com/kkenohh" className={styles.link} target="_blank"><FontAwesomeIcon icon={faGithub} size='2x' className={styles.icon} /></a>
+          </div>
+        </div>
+      </div>
+      <div 
+        className={clsx({
+          [styles.navLinks]: menuOpen === true,
+          [styles.menuHidden]: menuOpen === false
+        })}
+      >
+        <div className={styles.menuItem}><a className={styles.singleLink} href=''>About Me</a></div>
+        <div className={styles.menuItem}><a className={styles.singleLink} href=''>Projects</a></div>
+        <div className={styles.menuItem}><a className={styles.singleLink} href=''>Other</a></div>
       </div>
     </nav>
   )
--- portfolio-next/pages/index.js
+++ portfolio-next/pages/index.js
@@ -1,8 +1,15 @@
 import Layout from "@/components/layout";
+import Image from "next/image";
 
 export function Home() {
   return (
     <Layout>
+      <Image
+        src="/../public/images/file-1.jpeg"
+        height={900}
+        width={900}
+        alt="Legos"
+      />
     </Layout>
   )
 }
