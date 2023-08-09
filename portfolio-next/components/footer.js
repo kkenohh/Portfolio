f81c1ffc742ac9a18f3113614fdc95aebbd19d24
Ken <kenoh17@uw.edu>
2023-08-09 15:16:17 -0700

    started footer

---
+++ portfolio-next/components/footer.js
@@ -0,0 +1,18 @@
+import styles from '@/styles/footer.module.css'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import clsx from 'clsx'
+import { useState } from 'react';
+import Link from 'next/link'
+
+
+export default function Footer() {
+  
+
+  return (
+    <footer className={`${styles.footer}`}>
+      <div>hi</div>
+    </footer>
+  )
+}
