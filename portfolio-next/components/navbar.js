d0242e9bec797ec738e1f069c55b962cc6527c23
Ken <kenoh17@uw.edu>
2024-02-24 00:52:33 -0800

    fixed hamburger menu

--- portfolio-next/components/navbar.js
+++ portfolio-next/components/navbar.js
@@ -20,7 +20,7 @@ export default function Navbar() {
         <div className={styles.header}>
           <div className={styles.hamburger + ' ' + clsx({
             [styles.darker]: menuOpen === true,
-            [styles.normal]: menuOpen === false})} onClick={() => setMenuOpen(!menuOpen)}>
+            [styles.normal]: menuOpen === false})} onClick={() => updateMenu()}>
             <span className={styles.bars + ' ' + clsx({
             [styles.transform]: menuOpen === true,
             [styles.bars]: menuOpen === false})}></span>
@@ -31,25 +31,25 @@ export default function Navbar() {
             [styles.transform]: menuOpen === true,
             [styles.bars]: menuOpen === false})}></span>
           </div>
-          <div>
-            <div className={styles.socials}>
-              <a href="https://www.linkedin.com/in/ken-oh-64bb66239/" className={styles.link} target="_blank" aria-label='LinkedIn Link'><FontAwesomeIcon icon={faLinkedin} size='2x' className={styles.icon} /></a>
-              <a href="https://github.com/kkenohh" className={styles.link} target="_blank" aria-label='GitHub Link'><FontAwesomeIcon icon={faGithub} size='2x' className={styles.icon} /></a>
-            </div>
-          </div>
-        </div>
-        <div
-          className={clsx({
+          <div className={clsx({
             [styles.navLinks]: menuOpen === true,
-            [styles.menuHidden]: menuOpen === false
-          })}
-        >
-          <Link className={`${styles.singleLink} ${styles.menuItem}`} onClick={updateMenu} href='/'>Home</Link>
-          <Link className={`${styles.singleLink} ${styles.menuItem}`} onClick={updateMenu} href='/aboutMe'>About Me</Link>
-          <Link className={`${styles.singleLink} ${styles.menuItem}`} onClick={updateMenu} href='/projects'>Projects</Link>
-          <Link className={`${styles.singleLink} ${styles.menuItem}`} onClick={updateMenu} href='/other'>Other</Link>
+            [styles.menuHidden]: menuOpen === false})}>
+            <Link className={`${styles.singleLink} ${styles.menuItem}`} onClick={updateMenu} href='/'>Home</Link>
+            <Link className={`${styles.singleLink} ${styles.menuItem}`} onClick={updateMenu} href='/aboutMe'>About</Link>
+            <Link className={`${styles.singleLink} ${styles.menuItem}`} onClick={updateMenu} href='/projects'>Projects</Link>
+            <Link className={`${styles.singleLink} ${styles.menuItem}`} onClick={updateMenu} href='/other'>Other</Link>
+          </div>
+          <div className={styles.socials}>
+            <a href="https://www.linkedin.com/in/ken-oh-64bb66239/" className={styles.link} target="_blank" aria-label='LinkedIn Link'><FontAwesomeIcon icon={faLinkedin} size='2x' className={styles.icon} /></a>
+            <a href="https://github.com/kkenohh" className={styles.link} target="_blank" aria-label='GitHub Link'><FontAwesomeIcon icon={faGithub} size='2x' className={styles.icon} /></a>
+          </div>
         </div>
       </nav>
     </div>
   )
 }
+
+// styles.bars + ' ' + 
+
+// PROBLEM: LINES 35 and 36. When menuOpen is false, menuHidden is active, hiding the menu. I only want the menu to be hidden in the case of the hamburger icon being visible and closed. If the hamburger icon is not displayed, the menu should be visible at all times.
+// ^^ RESOLVED. There is redundancy in the navLinks and menuHidden classes of navbar.module.css, but should be fine.
