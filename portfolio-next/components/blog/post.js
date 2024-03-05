ee3648a248b169b5d7d2d740c2150ddea15f7b4f
Ken <kenoh17@uw.edu>
2024-03-05 03:48:56 -0800

    working on blog

--- portfolio-next/components/blog/post.js
+++ portfolio-next/components/blog/post.js
@@ -11,13 +11,7 @@ export default function Post() {
 
   return (
     <div className={styles.main}>
-      <div className={styles.header}>
-        <h1>Welcome to My Blog!</h1>
-        <p>This is just a nice page for me to post my hobbies and thoughts on different topics. From school to sports to photography, everything I enjoy will be here!</p>
-      </div>
-      <div className={styles.postsContainer}>
-
-      </div>
+      
     </div>
   )
 }
--- portfolio-next/pages/blog.js
+++ portfolio-next/pages/blog.js
@@ -1,1 +1,36 @@
 // Try to make it with multiple pages. So a set number of posts on one page, and then u have to click the arrow to go to the next. Also, sort the posts by tag or date (ascending or descending)
+import styles from '@/styles/post.module.css'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import clsx from 'clsx'
+import { useState } from 'react';
+import Link from 'next/link'
+
+
+export default function Blog() {
+  const [currPage, setCurrPage] = useState(0);
+  const [currPosts, setCurrPosts] = useState([]);
+
+  function updatePage() {
+    setCurrPage(currPage+10);
+  }
+
+  function updatePosts() {
+    setCurrPosts([]);
+  }
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <h1>Welcome to My Blog!</h1>
+        <p>This is just a nice page for me to post my hobbies and thoughts on different topics. From school to sports to photography, everything I enjoy will be here!</p>
+      </div>
+      <div className={styles.postsContainer}>
+
+      </div>
+    </div>
+  )
+}
+
+// date property should be Date object, however that works
